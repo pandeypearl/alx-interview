7a9cecb98cae070daede419fb8c5ae4a14a23b5c
Pretty Pandey <prettypandeypearl@gmail.com>
2023-07-04 00:37:35 -0700

    Task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,12 +7,14 @@ Lockboxes Solution
 def canUnlockAll(boxes):
     """
     Function to determine if all boxes can be unlocked.
+    Returns True if all boxes can be unlocked
+    and False otherwise
     """
-    keys = [0]
-    for key in keys:
-        for val in boxes[key]:
-            if val not in keys:
-                keys.append(val)
-    if len(keys) == len(boxes):
+    box_keys = [0]
+    for box_key in box_keys:
+        for value in boxes[box_key]:
+            if value not in box_keys:
+                box_keys.append(value)
+    if len(box_keys) == len(boxes):
         return True
     return False
