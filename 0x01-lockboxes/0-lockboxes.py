3fe82dc260107f43ac6dcfa0c3971f8449980e96
Pretty Pandey <prettypandeypearl@gmail.com>
2023-07-04 00:29:41 -0700

    Task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,11 +8,11 @@ def canUnlockAll(boxes):
     """
     Function to determine if all boxes can be unlocked.
     """
-    box_keys = [0]
-    for box_key in box_keys:
-        for value in boxes[box_key]:
-            if value not in box_keys:
-                box_keys.append(value)
-    if len(box_keys) == len(boxes):
+    keys = [0]
+    for key in keys:
+        for value in boxes[key]:
+            if value not in keys:
+                keys.append(value)
+    if len(keys) == len(boxes):
         return True
     return False
