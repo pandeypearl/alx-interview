625ee8b52964697d1c9f933efe7a02701cd85f64
Pretty Pandey <prettypandeypearl@gmail.com>
2023-07-04 01:54:04 -0700

    Task 0: Adding edge cases

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,18 +10,34 @@ def canUnlockAll(boxes):
     Returns True if all boxes can be unlocked
     and False otherwise
     """
-    box_keys = [0]
-    pending = []
-    for value in range(1, len(boxes)):
-        if value in box_keys:
-            for box_key in boxes[value]:
-                box_keys.append(box_key)
-        else:
-            pending.append(value)
-    for value in pending:
-        if value in box_keys:
-            for box_key in boxes[value]:
-                box_keys.append(box_key)
-        else:
+    if boxes is None:
+        return False
+    if not isinstance(boxes, list):
+        return False
+    if boxes == []:
+        return False
+    if boxes == [[]]:
+        return True
+    if boxes[0] == [] and len(boxes) > 1:
+        return False
+    for box_key in boxes:
+        if not isinstance(box_key, list):
             return False
-    return True
+        else:
+            for value in box_key:
+                if not isinstance(value, int):
+                    return False
+    Unlocked = [False for box_key in boxes]
+    Unlocked[0] = True
+    Open = [False for box_key in boxes]
+    while [True for box_key in range(len(boxes))
+            if Open[box_key] is False and Unlocked[box_key] is True]:
+        for i in range(len(boxes)):
+            if Unlocked[i] is True:
+                Open[i] = True
+                for box_key in boxes[i]:
+                    try:
+                        Unlocked[box_key] = True
+                    except Exception:
+                        pass
+    return False not in Unlocked
